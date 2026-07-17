611bd82ebba4dcfc2dc64dc9447c34d816b39fdb
Sam Welborn <swelborn@slac.stanford.edu>
2026-07-17 00:57:15 -0400

    generate python client

--- client/src/lclstream_api_client/_generated/api/__init__.py
+++ client/src/lclstream_api_client/_generated/api/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 
 # import apis into api package
+from lclstream_api_client._generated.api.caches_api import CachesApi
 from lclstream_api_client._generated.api.transfers_api import TransfersApi
 
